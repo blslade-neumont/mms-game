5afbe29c7fd0672992270f6d7772d0de9ff2c09d
Brandon <brandonyoyoslade@gmail.com>
2017-11-05 18:23:58 -0700

    feat(game): add basic player and camera movement

---
+++ M&Ms/Assets/Behaviors/CameraController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Camera))]
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] public GameObject follow;
+    [SerializeField] public float followDist = 8;
+    [SerializeField] public float raiseDist = 3;
+    private new Camera camera;
+
+    void Start()
+    {
+        this.camera = GetComponent<Camera>();
+    }
+
+    void Update()
+    {
+        var followPos = this.follow.transform.position;
+        var away = new Vector3(transform.position.x - followPos.x, 0, transform.position.z - followPos.z);
+        var normalizedAway = away.normalized * this.followDist;
+        this.transform.position = followPos + new Vector3(normalizedAway[0], raiseDist, normalizedAway[2]);
+        this.transform.LookAt(this.follow.transform);
+    }
+}
---
+++ M&Ms/Assets/Behaviors/PlayerController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+[RequireComponent(typeof(CharacterController))]
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] CameraController cameraController;
+    [SerializeField] float moveSpeed = 4;
+    [SerializeField] float jumpSpeed = 6;
+    [SerializeField] float gravityConst = 9.81f;
+
+    private CharacterController characterController;
+    private float vspeed;
+
+    void Start()
+    {
+        this.characterController = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        var horiz = Input.GetAxis("Horizontal");
+        var vert = Input.GetAxis("Vertical");
+        var jump = Input.GetButton("Jump");
+
+        var cam = cameraController.transform.forward;
+        var forward = new Vector3(cam[0], 0, cam[2]);
+        var sideways = Vector3.Cross(Vector3.up, forward);
+
+        this.vspeed -= this.gravityConst * Time.deltaTime;
+        var moveVector = ((forward * vert) + (sideways * horiz)) * this.moveSpeed;
+        if (this.characterController.isGrounded && jump) this.vspeed = this.jumpSpeed;
+        moveVector += new Vector3(0, this.vspeed, 0);
+        this.characterController.Move(moveVector * Time.deltaTime);
+        if (this.characterController.isGrounded) this.vspeed = 0;
+    }
+
+    private void OnDrawGizmos()
+    {
+        var cam = cameraController.transform.forward;
+        var forward = new Vector3(cam[0], 0, cam[2]);
+        var sideways = Vector3.Cross(Vector3.up, forward);
+
+        Gizmos.color = Color.red;
+        Gizmos.DrawRay(cameraController.transform.position, cam);
+        Gizmos.color = Color.blue;
+        Gizmos.DrawRay(this.transform.position, forward);
+        Gizmos.color = Color.green;
+        Gizmos.DrawRay(this.transform.position, sideways);
+    }
+}
